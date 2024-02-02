6211aea37f02621197e0eb911bd55cbdb6177882
CodeReDarsh <adarshcp2077@gmail.com>
2024-02-02 01:46:10 -0500

    doing this to see if it will let me pull

--- filetemp.py
+++ filetemp.py
@@ -2,3 +2,4 @@ for x in range(50):
     if x % 2 == 0:
         print(x)
 
+# some comment here
---
+++ temp2.py
@@ -0,0 +1,4 @@
+"""some more pythonic code here idk?
+it sounds pythonic it looks pythonic so it is pythonic
+;)"""
+
